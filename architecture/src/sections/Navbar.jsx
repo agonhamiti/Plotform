558eb38f9c395dd3d5d777bd2edbde03f6eb3eb1
AgonHamiti <98095419+agonhamiti@users.noreply.github.com>
2025-03-03 23:51:00 +0100

    Hot fix

--- architecture/src/sections/Navbar.jsx
+++ architecture/src/sections/Navbar.jsx
@@ -125,7 +125,7 @@ const Navbar = () => {
                   </svg>
                 </a>
                 <a 
-                  href="www.linkedin.com/company/plotformarch/" 
+                  href="https://www.linkedin.com/company/plotformarch/" 
                   target="_blank" 
                   rel="noopener noreferrer"
                   className="text-gray-600 hover:text-[#0A66C2] transition-colors"
